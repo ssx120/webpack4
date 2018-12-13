a14395ee04dc0943ed49414dbd5eab3d3857fb8b
cool <18721277347@163.com>
2018-12-13 14:23:25 +0800

    完善之前没有完善的

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.css'
-let a = 2 
- let  b = ()=>{
-
-}
-console.log("webpack1234567")
--- webpack.config.js
+++ webpack.config.js
@@ -1,95 +1,121 @@
-const path = require("path")
-var webpack = require("webpack")
-const htmlwebpackplugin = require("html-webpack-plugin")
-const extracttextwebpack = require("extract-text-webpack-plugin")
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
-  // 入口文件
-  entry: {
-    //   ["babel-polyfill",path.join(__dirname, './src/index.js') ],
-    app: "./src/index",
-    app2: "./src/index2"
+  mode: "development", // 开发模式
+  devtool: "source-map", // 开启调试
+  resolve: {
+    // // 别名
+    // alias: {
+    //   $: "./src/jquery.js"
+    // },
+    // 省略后缀
+    extensions: [".js", ".json", ".css"]
   },
-  // 出口文件
+  entry: ["@babel/polyfill", "./src/js/index.js"],
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  // 本地服務
-  devServer: {
-    contentBase: "src",
-    inline: true
-    // host:"0.0.0.0",
-    // hot:"8080"
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    // chunkFilename: "[name]-[hash:5].chunk.js",
+    publicPath: "./"
   },
   module: {
-    //关于模块配置
     rules: [
-      // js babel
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader"
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
         }
       },
-      // css样式loader
       {
         test: /\.css$/,
-        use: extracttextwebpack.extract({
-          fallback: "style-loader",
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                minimize: true
-              }
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
             }
-          ],
-          publicPath: "../"
-        })
+          },
+          "css-loader",
+          "postcss-loader"
+        ]
       },
-
-      // html内部可以使用img标签
       {
-        test: /\.html$/,
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192, // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+              outputPath: "images/" // 图片打包后存放的目录
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(htm|html)$/,
         use: "html-withimg-loader"
       },
-      //icon图标
       {
-        test: /\.(woff|ttf|svg|eot|xttf|woff2)$/,
-        use: "file-loader"
-      },
-      // css设置图片
-      {
-        test: /\.(jpg|png|jpeg|svg)$/,
+        test: /\.(eot|ttf|woff|svg)$/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[name].[ext]", //图片名字
-              publicPath: "../dist/images/", //图片存放的路径
-              outputPath: "images/" //将图片打包到dist文件下的image中
+              outputPath: "font/" // 打包后存放的目录
             }
           }
         ]
       }
     ]
   },
+  devServer: {
+    contentBase: "src",
+    host: "192.168.50.36", // 默认是localhost
+    open: true, // 自动打开浏览器
+    port: 8000, // 本地服务器端口号
+    hot: true, // 热重载
+    overlay: true, // 如果代码出错，会在浏览器页面弹出“浮动层”。类似于 vue-cli 等脚手架
+    proxy: {
+      // 跨域代理转发
+      //   "/comments": {
+      //     target: "https:xxx.com",
+      //     changeOrigin: true,
+      //     logLevel: "debug",
+      //     headers: {
+      //       Cookie: ""
+      //     }
+      //   }
+    },
+    historyApiFallback: {
+      // HTML5 history模式
+      //   rewrites: [{ from: /.*/, to: "/index.html" }]
+    }
+  },
   plugins: [
-    //插件
-    // css
-    new extracttextwebpack("./css/[name].css"),
-    // html插件
-    new htmlwebpackplugin({
+    // 打包前先清空
+    new CleanWebpackPlugin("dist"),
+    // 热替换，热替换不是刷新
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new UglifyJSPlugin(),
+    new HtmlWebpackPlugin({
+      // 用哪个html作为模板
+      // 在src目录下创建一个index.html页面当做模板来用
       template: "./src/index.html",
-      // filename:"index.html",重命名
-      minify: {
-        //去除多余的
-        removeAttributeQuotes: true, //去除引号
-        removeComments: true, //去除注释
-        removeEmptyAttributes: true, //去除空属性
-        collapseWhitespace: true //去除空格
-      }
+      hash: true, // 会在打包好的bundle.js后面加上hash串
+      filename: "./index.html",
+      //   chunks: ["app"]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash:8].css",
+      chunkFilename: "[id].css"
     })
   ]
+  // 这款插件用于压缩 JS 代码，减少资源体积大小
 };
